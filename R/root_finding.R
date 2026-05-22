d30480694d846666d6d95ed88ddbfb64dd042882
Your Name <you@example.com>
2026-05-22 20:47:52 +0000

    Fixed 2nd function

--- R/root_finding.R
+++ R/root_finding.R
@@ -1,18 +1,16 @@
 root_finding <- function(a,b,c){
-  print(sprintf("The input function is  %.2f x^2 +%.2f x +%.2f", a, b, c))
-  if (b^2-4*a*c <0){
-    print("The input function is irreducible and does not have a root.")
+  if ((b^2-4*a*c <0) || (a==0 && b==0 && c!=0)){
+    stop(paste("Invalid function or the input function is irreducible and does not have a root."))
   } else if (a ==0){
-    sprintf("The root of the input function is  x = %.2f.", -c/b)
+    -c/b
   } else {
     root1 <- (-b+ sqrt(b^2-4*a*c))/(2*a)
     root2 <- (-b- sqrt(b^2-4*a*c))/(2*a)
     if (root1 != root2){
-      sprintf("The roots of the input function is  x = %.2f and x = %.2f.", root1,root2)
+      c(root1, root2)
+    } else {
+      root1
     }
-    else {
-      sprintf("The roots of the input function is  x = %.2f.", root1)
-    }
-
   }
 }
+
